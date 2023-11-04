1c36fc9cc1816314e4fc0f20dbd9cc57bea97717
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-11-03 19:11:03 -0600

    Initial Commit

---
+++ Lokalito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lokalito
+//
+//  Created by Juan Ticante Vicente on 03/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
